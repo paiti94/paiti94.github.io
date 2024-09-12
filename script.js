f51ece59c3ec98a0153ff4acbc7ce6b7ba5a27d1
paiti94 <paiti94@gmail.com>
2024-09-12 15:18:34 -0700

    update animation

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tabLinks = document.querySelectorAll('.tab-link');
-    const tabContents = document.querySelectorAll('.tab-content');
-
-    tabLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            tabLinks.forEach(link => link.classList.remove('active'));
-            tabContents.forEach(content => content.classList.remove('active'));
-
-            link.classList.add('active');
-            document.getElementById(link.dataset.target).classList.add('active');
-        });
+    const typed = new Typed('#typed-output', {
+        strings: ['Senior Data Engineer','Fullstack Developer', 'AI Enthusiast', 'Technology Enthusiast'],
+        typeSpeed: 60,            // Speed in milliseconds for each character
+        backSpeed: 40,            // Speed in milliseconds to delete a character
+        backDelay: 2000,          // Time before backspacing
+        startDelay: 500,          // Time before typing starts
+        loop: true,               // Loop the typing animation
+        showCursor: true,         // Show the cursor blinking
+        cursorChar: '|',          // Character for cursor
+        autoInsertCss: true,      // Insert CSS for cursor & fadeOut into HTML
     });
 });
